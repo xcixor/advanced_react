005df6937987a028f6c9d2a410d4539a7f568aec
xcixor <pndungu54@gmail.com>
2021-06-30 03:14:53 +0300

    chore(app): prepare the app for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   resolve: {
@@ -7,13 +8,50 @@ module.exports = {
       path.resolve('./node_modules'),
     ]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.pickby',
+      'lodash.debounce'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        // vendor chunk
+        vendor: {
+          // async + async chunks
+          chunks: 'all',
+          // import file path containing node_modules
+          test: /node_modules/,
+          // priority
+          priority: 20
+        },
+        // common chunk
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 10,
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    }
+  }
 };
